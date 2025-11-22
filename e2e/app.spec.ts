74968814b67a933b8296c91711b25b5cfa85c19e
kgridou <32600911+kgridou@users.noreply.github.com>
2025-11-22 12:55:02 +0100

    playwright

---
+++ e2e/app.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Angular Dashboard', () => {
+  test('should display welcome message', async ({ page }) => {
+    await page.goto('/');
+
+    await expect(
+      page.getByRole('heading', { name: /Welcome to angular-dashboard!/i }),
+    ).toBeVisible();
+  });
+
+  test('should have correct title', async ({ page }) => {
+    await page.goto('/');
+
+    await expect(page).toHaveTitle(/angular-dashboard/i);
+  });
+});
